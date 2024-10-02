d576bb7d74e4329f7255a062ffa0eda861fb1b12
Mr-Bhardwa7 <animeshbhardwaj2@gmail.com>
2024-10-02 23:49:27 +0530

    feat: Create project mechanism implemented

--- lib/qwerty_cli.dart
+++ lib/qwerty_cli.dart
@@ -1,58 +1,37 @@
-// You have generated a new plugin project without specifying the `--platforms`
-// flag. A plugin project with no platform support was generated. To add a
-// platform, run `flutter create -t plugin --platforms <platforms> .` under the
-// same directory. You can also find a detailed instruction on how to add
-// platforms in the `pubspec.yaml` at
-// https://flutter.dev/docs/development/packages-and-plugins/developing-packages#plugin-platforms.
-
 import 'package:args/args.dart';
+import 'package:qwerty_cli/constants.dart';
+import 'package:qwerty_cli/utils/common_helpers.dart';
+import 'package:qwerty_cli/utils/project_helpers.dart';
 
 class QwertyCli {
   void main(List<String> arguments) {
     final parser = ArgParser()
-      ..addFlag(
-        'help',
-        abbr: 'h',
-        negatable: false,
-        help: 'Show help',
-      )
-      ..addOption(
-        'create-project',
-        abbr: 'c',
-        help: 'Create a new Flutter project',
-      )
-      ..addOption(
-        'state-management',
-        abbr: 's',
-        help: 'Choose state management option',
-        allowed: ['bloc', 'getX'],
-        mandatory: true,
-      );
+      ..addFlag(help, abbr: 'h', negatable: false, help: helpMessage)
+      ..addOption(create, abbr: 'c', help: createMessage)
+      ..addOption(stateManagement, abbr: 's', help: stateManagementMessage);
 
     ArgResults argResults = parser.parse(arguments);
 
+    final projectName = argResults[create];
+    final stateManagementOption = argResults[stateManagement];
+
     final commandMap = {
-      // ignore: avoid_print
-      'help': () => print(parser.usage),
-      'create-project': () {
-        var projectName =
-            argResults['create-project'] ?? 'qwerty_flutter_project';
-        createProject(projectName);
-      }
+      help: () => print(parser.usage),
+      create: () => createProject(projectName, stateManagementOption)
     };
 
-    if (argResults['help'] as bool) {
-      commandMap['help']!();
-    } else if (argResults.wasParsed('create-project')) {
-      commandMap['create-project']!();
-    } else {
-      // ignore: avoid_print
-      print('Unknown command. Use  --help to see available options.');
-    }
-  }
+    if (argResults[help] as bool) {
+      commandMap[help]!();
+    } else if (argResults.wasParsed(create)) {
+      if (projectName == null || stateManagementOption == null) {
+        printMessage(suggestionMessage, color: yellow);
+        return;
+      }
 
-  void createProject(String projectName) {
-    // ignore: avoid_print
-    print('Creating Flutter project: $projectName');
+      checkDependencies();
+      commandMap[create]!();
+    } else {
+      printMessage(unknownCommand, color: red);
+    }
   }
 }
